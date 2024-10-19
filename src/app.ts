eed20e02884cbab9140d640fd4766e4798889bd5
GMSzebeni <gabriella.szebeni@gmail.com>
2024-10-20 00:30:05 +0200

    add solutions for tasak 2 and 4

--- src/app.ts
+++ src/app.ts
@@ -7,5 +7,18 @@ export default function createApp(options = {}) {
     reply.send({hello: "World!"})
   })
 
+  app.get('/api/good-bye', (request, reply) => {
+    reply.code(200).send({ message: "Good Bye Visitor!" });
+  })
+
+  type PostBeverageRouteType = {
+    Params: { beverage: "coffee" | "tea" | "chai" };
+  }
+
+  app.post<PostBeverageRouteType>('/api/beverages/:beverage', (request, reply) => {
+    const { beverage } = request.params;
+    reply.code(200).send({ drink: beverage });
+  })
+
   return app;
 }
