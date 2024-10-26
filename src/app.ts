2f91bbeae2b6c9ca6e5199e38c7a4ee2b6bd1867
GMSzebeni <gabriella.szebeni@gmail.com>
2024-10-26 16:44:57 +0200

    add solution for task 7

--- src/app.ts
+++ src/app.ts
@@ -15,15 +15,26 @@ export default function createApp(options = {}) {
     Querystring: { milk?: "yes" | "no", sugar?: "yes" | "no" };
     Params: { beverage: "coffee" | "tea" | "chai" };
     Body: { kind?: "string" }
+    Headers: { 'CodeCool-Beverages-Dietary'?: 'lactose-intolerance' | 'vegan' }
   }
 
   app.post<PostBeverageRouteType>('/api/beverages/:beverage', (request, reply) => {
     const { beverage } = request.params;
     const { milk, sugar } = request.query;
     const { kind } = request.body;
+    const diet = request.headers['codecool-beverages-dietary'];
+    console.log(diet);
 
     const condiments = [];
-    if(milk === "yes") condiments.push("milk");
+    if(milk === "yes") {
+      if (diet === "lactose-intolerance") {
+        condiments.push("lf-milk");
+      } else if (diet === "vegan") {
+        condiments.push("oat-milk");
+      } else {
+        condiments.push("milk");
+      }
+    }
     if (sugar === "yes") condiments.push("sugar");
 
     reply.code(200).send({ drink: kind + " " + beverage, with: condiments });
