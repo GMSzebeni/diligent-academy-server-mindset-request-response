6575b2d0e358ccf33091d5eed4fd2a73f9ebdb08
GMSzebeni <gabriella.szebeni@gmail.com>
2024-10-28 20:19:54 +0100

    add solution to tasks 10, 11 & 12

--- src/app.ts
+++ src/app.ts
@@ -11,42 +11,77 @@ export default function createApp(options = {}) {
     reply.code(200).send({ message: "Good Bye Visitor!" });
   })
 
+  const schema = {
+    params: {
+      type: "object",
+      properties: {
+        beverage: { enum: ["coffee", "tea", "chai"] }
+      },
+      required: ["beverage"],
+      additionalProperties: false
+    },
+    body: {
+      content: {
+        'application/json': {
+          schema: {
+            type: "object"
+          }
+        }
+      },
+      required: ["kind"]
+    },
+    queryString: {
+      type: "object",
+      properties: {
+        milk: {
+          type: "string"
+        },
+        sugar: {
+          type: "string"
+        }
+      }
+    }
+  }
+
   type PostBeverageRouteType = {
     Querystring: { milk?: "yes" | "no", sugar?: "yes" | "no" };
     Params: { beverage: "coffee" | "tea" | "chai" };
-    Body: { kind?: "string" }
-    Headers: { 'CodeCool-Beverages-Dietary'?: 'lactose-intolerance' | 'vegan' }
+    Body: { kind: "string" };
+    Headers: { 'CodeCool-Beverages-Dietary'?: 'lactose-intolerance' | 'vegan' };
   }
 
-  app.post<PostBeverageRouteType>('/api/beverages/:beverage', (request, reply) => {
-    const { beverage } = request.params;
-    if (!beverage || beverage !== "coffee" && beverage !== "tea" && beverage !== "chai") {
-      reply.code(400).send({reason: 'bad drink'});
-    }
-    const { milk, sugar } = request.query;
-    //const { kind } = request.body;
-    const diet = request.headers['codecool-beverages-dietary'];
+  app.post<PostBeverageRouteType>(
+    '/api/beverages/:beverage',
+    {
+      schema: schema
+    },
+    (request, reply) => {
+      const { beverage } = request.params;
+      const { milk, sugar } = request.query;
+      const { kind } = request.body;
+      const diet = request.headers['codecool-beverages-dietary'];
 
-    const condiments = [];
-    if(milk === "yes") {
-      if (diet === "lactose-intolerance") {
-        condiments.push("lf-milk");
-      } else if (diet === "vegan") {
-        condiments.push("oat-milk");
+      const condiments = [];
+      if(milk === "yes") {
+        if (diet === "lactose-intolerance") {
+          condiments.push("lf-milk");
+        } else if (diet === "vegan") {
+          condiments.push("oat-milk");
+        } else {
+          condiments.push("milk");
+        }
+      }
+      if (sugar === "yes") condiments.push("sugar");
+
+      const replyMessage = { drink: kind + " " + beverage, with: condiments }
+      
+      if (beverage === "coffee") {
+        reply.code(418).send(replyMessage);
       } else {
-        condiments.push("milk");
+        reply.code(201).send(replyMessage);
       }
     }
-    if (sugar === "yes") condiments.push("sugar");
-
-    const replyMessage = { drink: /*kind + " " + */beverage, with: condiments }
-    
-    if (beverage === "coffee") {
-      reply.code(418).send(replyMessage);
-    } else {
-      reply.code(201).send(replyMessage);
-    }
-  })
+  )
 
   return app;
 }
